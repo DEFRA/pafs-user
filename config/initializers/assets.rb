e911c1b0cecedecfd70eb47746b3d806866ebaea
Tony Headford <tony@binarycircus.com>
2016-03-10 15:23:53 +0000

    Adding in WEX stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( gov.uk_logotype_crown.svg )
+Rails.application.config.assets.precompile += %w( gov.uk_logotype_crown.svg application-ie6.css application-ie7.css application-ie8.css )
