004b1b975599050feead08f5c99b9826bbc48e59
Tony Headford <tony@binarycircus.com>
2016-03-18 09:54:13 +0000

    Setting up layout and styles

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,11 @@ module Pafs
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # load files from nested directories
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # add any locale files from pafs_core
+    config.i18n.load_path += Dir["#{%x[bundle show pafs_core].chomp}/config/locales/**/*.yml"]
+
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
