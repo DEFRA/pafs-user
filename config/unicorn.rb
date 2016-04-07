3a174ad67943e48c60fba26528d989ffde44680f
Chris Gannon <cjgannon@gmail.com>
2016-04-07 10:17:13 +0100

    updated kill signals

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,28 @@
 worker_processes 3
 timeout 15
 preload_app true
+pid "#{SHARED_ROOT}/tmp/pids/unicorn.pid"
+
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
 end
