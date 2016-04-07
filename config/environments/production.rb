95fdc1b1639375e95843ac1c4c7059cb52d7ceed
Chris Gannon <cjgannon@gmail.com>
2016-04-07 15:52:43 +0100

    changed precompile line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :scss
+  # config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
