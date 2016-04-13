b5c933fdaf284b9baa72d4b0fc49ed48b62066c6
Chris Gannon <cjgannon@gmail.com>
2016-04-13 16:45:10 +0100

    updated smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => ENV["EMAIL_HOST"],
+    :port                 => ENV["EMAIL_PORT"]
+  }
 end
