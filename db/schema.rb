0e686723ecef88f4469b99e667cd241d45d5642b
Chris Gannon <cjgannon@gmail.com>
2016-06-06 10:42:17 +0100

    updated pafs core user migration

--- db/schema.rb
+++ db/schema.rb
@@ -128,25 +128,25 @@ ActiveRecord::Schema.define(version: 20160602121500) do
   add_index "pafs_core_user_areas", ["area_id"], name: "index_pafs_core_user_areas_on_area_id", using: :btree
   add_index "pafs_core_user_areas", ["user_id"], name: "index_pafs_core_user_areas_on_user_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "pafs_core_users", force: :cascade do |t|
+    t.string   "email",                         default: "", null: false
+    t.string   "encrypted_password",            default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "remember_created_apafs_core_t"
+    t.integer  "sign_in_count",                 default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.integer  "failed_attempts",        default: 0,  null: false
+    t.integer  "failed_attempts",               default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
+    t.string   "first_name",                    default: "", null: false
+    t.string   "last_name",                     default: "", null: false
     t.string   "job_title"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -154,14 +154,14 @@ ActiveRecord::Schema.define(version: 20160602121500) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
-    t.integer  "invitations_count",      default: 0
+    t.integer  "invitations_count",             default: 0
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
-  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+  add_index "pafs_core_users", ["email"], name: "index_pafs_core_users_on_email", unique: true, using: :btree
+  add_index "pafs_core_users", ["invitation_token"], name: "index_pafs_core_users_on_invitation_token", unique: true, using: :btree
+  add_index "pafs_core_users", ["invitations_count"], name: "index_pafs_core_users_on_invitations_count", using: :btree
+  add_index "pafs_core_users", ["invited_by_id"], name: "index_pafs_core_users_on_invited_by_id", using: :btree
+  add_index "pafs_core_users", ["reset_password_token"], name: "index_pafs_core_users_on_reset_password_token", unique: true, using: :btree
+  add_index "pafs_core_users", ["unlock_token"], name: "index_pafs_core_users_on_unlock_token", unique: true, using: :btree
 
 end
