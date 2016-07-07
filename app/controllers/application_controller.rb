5ca1be817d7d65f13af78f323b9eed769be8eef1
Tony Headford <tony@binarycircus.com>
2016-07-07 14:51:47 +0100

    Updating signin page content (PK-494)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # Play nice with Ruby 3 (and rubocop)
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  helper PafsCore::Engine.helpers
    # This is a nested template that ultimately calls the gov uk template layout
   # This allows us to auto insert code into any of the hooks provided by the GDS layout.
   layout ->(_) { "pafs" }
