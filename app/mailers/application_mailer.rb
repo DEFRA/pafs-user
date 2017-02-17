5886f43d3b6d0d11d5da18747bc39b46820c0330
Tony Headford <tony@binarycircus.com>
2017-02-17 10:51:55 +0000

    Removing tracking links in email (PM-242)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class ApplicationMailer < ActionMailer::Base
+  include PafsCore::Email
+  add_template_helper(PafsCore::EmailHelper)
+  layout "mailer"
   default from: ENV.fetch("DEVISE_MAILER_SENDER")
   layout "mailer"
 end
---
+++ app/mailers/pafs_mailer.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+class PafsMailer < Devise::Mailer
+  include PafsCore::Email
+  add_template_helper(PafsCore::EmailHelper)
+  include Devise::Controllers::UrlHelpers
+  layout "mailer"
+  default template_path: "devise/mailer"
+
+  def confirmation_instructions(record, token, opts = {})
+    prevent_tracking
+    super
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    prevent_tracking
+    super
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    prevent_tracking
+    super
+  end
+
+  def password_change(record, opts = {})
+    prevent_tracking
+    super
+  end
+end
