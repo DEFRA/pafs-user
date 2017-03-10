3d365444794f2ea213436916927889d248177a39
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2017-03-10 16:11:11 +0000

    Add CSP to response headers
    Following a PEN test it was flagged that the service has not specified a [Content Security Policy](https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP) in the response headers sent back from the app to the clients browser.
    
    For reference CSP
    
    > [..] is an HTTP response header that restricts the browser to loading external assets such as scripts, styles or media from a wide variety of sources — as well as inline scripts. It is intended to prevent wide categories of attacks, such as cross-site scripting (XSS), click-jacking and other forms of code injection.
    >
    > *https://blog.sqreen.io/integrating-content-security-policy-into-your-rails-applications-4f883eed8f45/*
    
    The actual code to customise the response headers is held in [pafs_core](https://github.com/DEFRA/pafs_core/pull/128) so this change updates the `ApplicationController` to pull the `PafsCore::CustomHeaders` module in and ensure all pages that originate from **pafs-user** also apply the CSP.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 # Play nice with Ruby 3 (and rubocop)
 # frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  include PafsCore::CustomHeaders
+
   helper PafsCore::Engine.helpers
-   # This is a nested template that ultimately calls the gov uk template layout
+
+  # This is a nested template that ultimately calls the gov uk template layout
   # This allows us to auto insert code into any of the hooks provided by the GDS layout.
   layout ->(_) { "pafs" }
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :custom_headers
+
+  private
+
+  def custom_headers
+    response_headers!(response)
+  end
+
 end
