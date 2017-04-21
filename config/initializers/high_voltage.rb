97701a18247a13f9b4971b368896f50258562419
Tony Headford <tony@binarycircus.com>
2017-04-21 13:59:21 +0100

    Removed start page and now root to dashboard

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,5 +1,6 @@
 HighVoltage.configure do |config|
   # This is a nested template that ultimately calls the gov uk template layout
   config.layout = 'pafs'
-  config.home_page = 'start'
+  # remove start page as root '/'
+  # config.home_page = 'start'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root to: 'pafs_core/projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
