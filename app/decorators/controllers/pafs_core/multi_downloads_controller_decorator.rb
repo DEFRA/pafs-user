ebe5800ff488ccd3ec1991ddfa8cf39017bc09c4
Tony Headford <tony@binarycircus.com>
2017-04-25 12:07:54 +0100

    Added decorator for multi-project downloads controller (PM-297)

---
+++ app/decorators/controllers/pafs_core/multi_downloads_controller_decorator.rb
@@ -0,0 +1,9 @@
+# Play nice with Ruby 3 (and rubocop)
+# frozen_string_literal: true
+PafsCore::MultiDownloadsController.class_eval do
+  helper ::ApplicationHelper
+  layout ->(_) { "pafs" }
+
+  # adding this here as pafs_core doesn't know about Devise
+  before_action :authenticate_user!
+end
