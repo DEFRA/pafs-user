5adde01faeab526f4fdc087a5d057cf02c84620a
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-05-17 15:22:21 +0100

    [PM-377] Refactor view spec to use a table
    * Lots of repetitive tests. This cuts down on the code

--- spec/features/projects/viewing_spec.rb
+++ spec/features/projects/viewing_spec.rb
@@ -1,223 +1,92 @@
 # frozen_string_literal: true
 
+
+FLAG_DISABLED_SPECS = [
+  { user_type: :rma, project_area: :pso_area, project_state: :draft,    can_change_state: true },
+  { user_type: :rma, project_area: :rma_area, project_state: :draft,    can_change_state: true },
+  { user_type: :rma, project_area: :pso_area, project_state: :archived, can_change_state: true },
+  { user_type: :rma, project_area: :rma_area, project_state: :archived, can_change_state: true },
+  { user_type: :pso, project_area: :pso_area, project_state: :draft,    can_change_state: true },
+  { user_type: :pso, project_area: :rma_area, project_state: :draft,    can_change_state: true },
+  { user_type: :pso, project_area: :pso_area, project_state: :archived, can_change_state: true },
+  { user_type: :pso, project_area: :rma_area, project_state: :archived, can_change_state: true },
+]
+
+FLAG_ENABLED_SPECS = [
+  { user_type: :rma, project_area: :pso_area, project_state: :draft,    can_change_state: false },
+  { user_type: :rma, project_area: :rma_area, project_state: :draft,    can_change_state: true },
+  { user_type: :rma, project_area: :pso_area, project_state: :archived, can_change_state: false },
+  { user_type: :rma, project_area: :rma_area, project_state: :archived, can_change_state: true },
+  { user_type: :pso, project_area: :pso_area, project_state: :draft,    can_change_state: false },
+  { user_type: :pso, project_area: :rma_area, project_state: :draft,    can_change_state: true },
+  { user_type: :pso, project_area: :pso_area, project_state: :archived, can_change_state: false },
+  { user_type: :pso, project_area: :rma_area, project_state: :archived, can_change_state: true },
+]
+
 RSpec.feature 'Viewing a project', type: :feature do
   context 'without FORCE_PSO_TO_POL set' do
-    context 'as an rma user' do
-      let(:user) { create(:account_user, :rma) }
-
-      before do
-        login_as(user)
-      end
-
-      context 'with a draft project for an rma area' do
-        let(:project) { create(:full_project) }
+    FLAG_DISABLED_SPECS.each do |spec|
+      context "as a #{spec[:user_type]} user" do
+        let(:user) { create(:account_user, spec[:user_type]) }
 
         before do
-          project.area_projects.create(area: user.primary_area)
+          login_as(user)
         end
 
-        scenario 'I can submit the project' do
-          view_a_project(project)
+        context "with a #{spec[:project_state]} project for a #{spec[:project_area]}" do
+          let(:project) { create(:full_project) }
+          let(:area) { create(spec[:project_area]) }
 
-          expect(page).to have_selector('a', text: 'Submit')
-        end
+          before do
+            project.area_projects.create(area: area)
+            user.user_areas.create(area: area)
+          end
 
-        scenario 'I can archive the project' do
-          view_a_project(project)
+          if spec[:can_change_state]
+            scenario 'I cannot submit the project' do
+              view_a_project(project)
 
-          expect(page).to have_selector('.project-overview-head a', text: 'Archive')
-        end
-      end
+              expect(page).not_to have_selector('a', text: 'Submit')
+            end
 
-      context 'with an archived project for an rma area' do
-        let(:project) { create(:full_project, :archived) }
+            scenario 'I cannot un-archive the project' do
+              view_a_project(project)
 
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
+              expect(page).not_to have_selector('a', text: 'Revert to draft')
+            end
 
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
+            scenario 'I cannot archive the project' do
+              view_a_project(project)
 
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
+              expect(page).not_to have_selector('.project-overview-head a', text: 'Archive')
+            end
+          else
+            if spec[:project_state] == :archived
+              scenario 'I cannot submit the project' do
+                view_a_project(project)
 
-        scenario 'I can un-archive the project' do
-          view_a_project(project)
+                expect(page).not_to have_selector('a', text: 'Submit')
+              end
 
-          expect(page).to have_selector('a', text: 'Revert to draft')
-        end
-      end
-    end
+              scenario 'I can un-archive the project' do
+                view_a_project(project)
 
-    context 'as a pso user' do
-      let(:user) { create(:account_user, :pso) }
+                expect(page).to have_selector('a', text: 'Revert to draft')
+              end
+            else
+              scenario 'I can submit the project' do
+                view_a_project(project)
 
-      before do
-        login_as(user)
-      end
+                expect(page).to have_selector('a', text: 'Submit')
+              end
 
-      context 'with a draft project for a pso area' do
-        let(:project) { create(:full_project) }
+              scenario 'I can archive the project' do
+                view_a_project(project)
 
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I can submit the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I can archive the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('.project-overview-head a', text: 'Archive')
-        end
-      end
-
-      context 'with an archived project for an rma area' do
-        let(:project) { create(:full_project, :archived) }
-
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I can un-archive the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('a', text: 'Revert to draft')
-        end
-      end
-    end
-  end
-
-  context 'with FORCE_PSO_TO_POL set' do
-    before do
-      allow(ENV).to receive(:fetch).with('FORCE_PSO_TO_POL', false).and_return(true)
-    end
-
-    context 'as an rma user' do
-      let(:user) { create(:account_user, :rma) }
-
-      before do
-        login_as(user)
-      end
-
-      context 'with a draft project for an rma area' do
-        let(:project) { create(:full_project) }
-
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I can submit the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I can archive the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('.project-overview-head a', text: 'Archive')
-        end
-      end
-
-      context 'with a draft project for an pso area' do
-        let(:project) { create(:full_project) }
-        let(:pso_area) { create(:pso_area) }
-
-        before do
-          user.user_areas.create(area: pso_area)
-          project.area_projects.create(area: pso_area)
-        end
-
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I cannot archive the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('.project-overview-head a', text: 'Archive')
-        end
-      end
-
-      context 'with an archived project for an rma area' do
-        let(:project) { create(:full_project, :archived) }
-
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I can un-archive the project' do
-          view_a_project(project)
-
-          expect(page).to have_selector('a', text: 'Revert to draft')
-        end
-      end
-    end
-
-    context 'as a pso user' do
-      let(:user) { create(:account_user, :pso) }
-
-      before do
-        login_as(user)
-      end
-
-      context 'with a draft project for a pso area' do
-        let(:project) { create(:full_project) }
-
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I can archive the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('.project-overview-head a', text: 'Archive')
-        end
-      end
-
-      context 'with an archived project for an rma area' do
-        let(:project) { create(:full_project, :archived) }
-
-        before do
-          project.area_projects.create(area: user.primary_area)
-        end
-
-        scenario 'I cannot submit the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Submit')
-        end
-
-        scenario 'I cannot un-archive the project' do
-          view_a_project(project)
-
-          expect(page).not_to have_selector('a', text: 'Revert to draft')
+                expect(page).to have_selector('.project-overview-head a', text: 'Archive')
+              end
+            end
+          end
         end
       end
     end
