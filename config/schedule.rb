2de96358e6ac7ba87694814b266a1b13130dc393
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-08-23 16:32:21 +0100

    Update rake template to load the app environment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+job_type :rake, "cd :path && . ../../.exportedenv && :environment_variable=:environment bundle exec rake :task --silent :output"
+
 every 1.day, at: "1:00 am" do
   rake "users:clean"
 end
