94d7cb01c08797e80bbda36cc68c93a6d54d47a2
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2019-08-27 10:51:26 +0100

    Disable job that's not been run for a while until it can be verified as safe

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 job_type :rake, "cd :path && . ../../.exportedenv && :environment_variable=:environment bundle exec rake :task --silent :output"
 
-every 1.day, at: "1:00 am" do
-  rake "users:clean"
-end
+# Disabling this for now as it has not been running for a long time
+#every 1.day, at: "1:00 am" do
+#  rake "users:clean"
+#end
