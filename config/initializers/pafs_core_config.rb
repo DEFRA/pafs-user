56b8a68759800de73ac1e6297ae9b959458255ef
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-07-20 12:41:38 +0100

    Configure defra ruby alerts

--- config/initializers/pafs_core_config.rb
+++ config/initializers/pafs_core_config.rb
@@ -1,4 +1,10 @@
 PafsCore.configure do |config|
   config.complete_page_feedback_uri = "https://www.gov.uk/done/flood-and-coastal-defence-funding-submit-a-project"
   config.banner_feedback_uri = "http://www.smartsurvey.co.uk/s/PAFSGDS/"
+
+  # Configure airbrake, which is done via the engine using defra_ruby_alert
+  config.airbrake_enabled = ENV.key?("AIRBRAKE_PROJECT_KEY")
+  config.airbrake_host = ENV["AIRBRAKE_HOST"]
+  config.airbrake_project_key = ENV["AIRBRAKE_PROJECT_KEY"]
+  config.airbrake_blocklist = [/password/i, /authorization/i]
 end
