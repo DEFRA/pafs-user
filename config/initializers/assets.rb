7222361934de18ee1e77ff488c3bf2db606bd42e
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-10-26 16:24:32 +0100

    rubocop fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,8 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w[
-    gov.uk_logotype_crown.svg
-    application-ie6.css
-    application-ie7.css
-    application-ie8.css
-  ]
+  gov.uk_logotype_crown.svg
+  application-ie6.css
+  application-ie7.css
+  application-ie8.css
+]
