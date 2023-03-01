6e60ae05e781e057fbb55dd68dc586785b1a3222
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-03-01 16:23:10 +0000

    Add rake task for changing RMAs based on a CSV file

--- spec/lib/tasks/import_areas_spec.rb
+++ spec/lib/tasks/import_areas_spec.rb
@@ -4,11 +4,11 @@ require "rails_helper"
 
 RSpec.describe "areas", type: :rake do
 
+  include_context "rake"
+
   describe "areas:import_additional_areas", type: :rake do
 
     subject(:areas_task) { Rake::Task["areas:import_additional_areas"] }
-
-    include_context "rake"
 
     let(:importer) { instance_double(PafsCore::AreaImporter) }
 
@@ -28,12 +28,31 @@ RSpec.describe "areas", type: :rake do
 
     subject(:areas_task) { Rake::Task["areas:rename_area"] }
 
-    include_context "rake"
-
     before { create(:rma_area, name: "foo") }
 
     it "runs without error" do
       expect { areas_task.invoke("RMA", "foo", "bar") }.not_to raise_error
+    end
+  end
+
+  describe "areas:change_areas", type: :rake do
+
+    subject(:areas_task) { Rake::Task["areas:change_areas"] }
+
+    before do
+      a1 = create(:rma_area, name: "Allerdale Borough Council")
+      a2 = create(:rma_area, name: "Barrow-in-Furness Borough Council")
+      a3 = create(:rma_area, name: "Copeland Borough Council")
+      create(:rma_area, name: "Cumberland Council")
+      create(:rma_area, name: "Westmorland and Furness Council")
+
+      create(:project, reference_number: "NWC501E/000A/042A", areas: [a1])
+      create(:project, reference_number: "NWC501E/000A/570A", areas: [a2])
+      create(:project, reference_number: "NWC501E/000A/614A", areas: [a3])
+    end
+
+    it "runs without error" do
+      expect { areas_task.invoke("spec/fixtures/rma_changes.csv") }.not_to raise_error
     end
   end
 end
