036648bb04f0647a10daecd6ce339afddf908a47
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-04-06 14:05:44 +0100

    Use custom matcher for viewing_spec

--- spec/features/projects/viewing_spec.rb
+++ spec/features/projects/viewing_spec.rb
@@ -30,19 +30,17 @@ FLAG_ENABLED_SPECS = [
   { user_type: :pso, project_area: :rma_area, project_state: :archived,  can_change_state: true,  can_edit: false }
 ].freeze
 
-RSpec.shared_examples "can do" do |action, selectors|
-  it "I can #{action} the project" do
-    selectors.each do |sel|
-      expect(page).to have_selector("a", text: sel)
-    end
+RSpec::Matchers.define :have_ability_to do |action_label, selector_classes: "a"|
+  match do |page|
+    page.has_selector?(selector_classes, text: action_label)
   end
-end
 
-RSpec.shared_examples "cannot do" do |action, selectors, selector_class|
-  it "I cannot #{action} the project" do
-    selectors.each do |sel|
-      expect(page).not_to have_selector(selector_class, text: sel)
-    end
+  failure_message do
+    "expected page to have the ability to #{action_label} the project, but it does not"
+  end
+
+  failure_message_when_negated do
+    "expected page not to have the ability to #{action_label} the project, but it does"
   end
 end
 
@@ -50,9 +48,7 @@ RSpec.shared_examples "viewing a project" do |spec|
   context "with a #{spec[:user_type]} user" do
     let(:user) { create(:account_user, spec[:user_type]) }
 
-    before do
-      login_as(user)
-    end
+    before { login_as(user) }
 
     context "with a #{spec[:project_state]} project for a #{spec[:project_area]}" do
       let(:project) { create(:full_project, spec[:project_state]) }
@@ -66,27 +62,35 @@ RSpec.shared_examples "viewing a project" do |spec|
         view_a_project(project)
       end
 
-      it_behaves_like spec[:can_edit] ? "can do" : "cannot do", "edit", %w[Change Add], "a"
+      # rubocop:disable RSpec/RepeatedExample
+      if spec[:can_edit]
+        it { expect(page).to have_ability_to("Change") }
+        it { expect(page).to have_ability_to("Add") }
+      else
+        it { expect(page).not_to have_ability_to("Change") }
+        it { expect(page).not_to have_ability_to("Add") }
+      end
 
       if spec[:can_change_state]
         if spec[:project_state] == :archived
-          it_behaves_like "cannot do", "submit", %w[Submit], "a"
-          it_behaves_like "can do", "revert", ["Revert to draft"], "a"
+          it { expect(page).not_to have_ability_to("Submit") }
+          it { expect(page).to have_ability_to("Revert to draft") }
         end
 
         if spec[:project_state] == :draft
-          it_behaves_like "can do", "submit", %w[Submit], "a"
-          it_behaves_like "can do", "archive", %w[Archive], ".project-overview-head a"
+          it { expect(page).to have_ability_to("Submit") }
+          it { expect(page).to have_ability_to("Archive", selector_classes: ".project-overview-head a") }
         end
 
         if spec[:project_state] == :submitted # rubocop:disable Style/IfUnlessModifier
-          it_behaves_like "can do", "revert", ["Revert to draft"], "a"
+          it { expect(page).to have_ability_to("Revert to draft") }
         end
       else
-        it_behaves_like "cannot do", "submit", %w[Submit], "a"
-        it_behaves_like "cannot do", "revert", ["Revert to draft"], "a"
-        it_behaves_like "cannot do", "archive", %w[Archive], ".project-overview-head a"
+        it { expect(page).not_to have_ability_to("Submit") }
+        it { expect(page).not_to have_ability_to("Revert to draft") }
+        it { expect(page).not_to have_ability_to("Archive", selector_classes: ".project-overview-head a") }
       end
+      # rubocop:enable RSpec/RepeatedExample
     end
   end
 end
