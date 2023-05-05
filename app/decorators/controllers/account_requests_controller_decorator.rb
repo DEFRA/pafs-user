56095bb3a01a7d44984bb2d5b2282b65f14a3ccb
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-05-05 12:23:43 +0100

    realign with remote branch

--- app/decorators/controllers/account_requests_controller_decorator.rb
+++ app/decorators/controllers/account_requests_controller_decorator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-PafsCore::AccountRequestsController.class_eval do
+AccountRequestsController.class_eval do
   helper ApplicationHelper
   layout ->(_) { "pafs" }
 end
