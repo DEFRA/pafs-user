f9b29158ba0dd17cbb551cfd821409623b1e7ed1
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-05-05 12:37:18 +0100

    namespace tweak for locally-defined controller decorator

--- app/decorators/controllers/account_requests_controller_decorator.rb
+++ app/decorators/controllers/account_requests_controller_decorator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-AccountRequestsController.class_eval do
+::AccountRequestsController.class_eval do # rubocop:disable Style/RedundantConstantBase
   helper ApplicationHelper
   layout ->(_) { "pafs" }
 end
