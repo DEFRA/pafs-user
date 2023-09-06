69ce3baca8829c239a8b44511dbb87731d80277e
Leonid Brujev <brujev@gmail.com>
2023-09-06 18:25:06 +0100

    code styling fixes

--- spec/lib/tasks/projects_spec.rb
+++ spec/lib/tasks/projects_spec.rb
@@ -1,17 +1,19 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe 'projects' do
+require "rails_helper"
 
-  describe 'projects:import', type: :rake do
-    let(:area1) { create(:rma_area, name: 'Council 1') }
-    let(:area2) { create(:rma_area, name: 'Council 2') }
-    let(:csv_file) { Rails.root.join('tmp', 'projects.csv') }
+RSpec.describe "projects" do
+
+  describe "projects:import", type: :rake do
+    let(:area_one) { create(:rma_area, name: "Council 1") }
+    let(:area_two) { create(:rma_area, name: "Council 2") }
+    let(:csv_file) { Rails.root.join("tmp/projects.csv") }
 
     before do
       # create a CSV file with test data
-      CSV.open(csv_file, 'wb') do |csv|
-        csv << ['Project 1', 'ANC401I/000A/007A', area1.name]
-        csv << ['Project 2', 'ANC401I/000A/008A', area1.name]
+      CSV.open(csv_file, "wb") do |csv|
+        csv << ["Project 1", "ANC401I/000A/007A", area_one.name]
+        csv << ["Project 2", "ANC401I/000A/008A", area_two.name]
       end
     end
 
@@ -20,32 +22,37 @@ RSpec.describe 'projects' do
       File.delete(csv_file)
     end
 
-    it 'imports projects from a CSV file' do
-      expect { Rake::Task['projects:import'].invoke }.to output(/../).to_stdout
+    it "imports projects from a CSV file" do
+      expect { Rake::Task["projects:import"].invoke }.to output(/../).to_stdout
     end
   end
 
-  describe 'projects:delete_all', type: :rake do
-    let(:task_name) { 'projects:delete_all' }
-    let(:storage_path) { Rails.root.join('storage') }
-  
+  describe "projects:delete_all", type: :rake do
+    let(:task_name) { "projects:delete_all" }
+    let(:storage_path) { Rails.root.join("storage") }
+
     before do
       # create some test projects
       create(:full_project)
     end
-  
+
     after do
       # delete all projects after the test
       PafsCore::Project.delete_all
     end
-  
-    it 'deletes all projects and their associated files' do
+
+    it "deletes all projects" do
       # run the task
       Rake::Task[task_name].invoke
-  
+
       # check if all projects were deleted
       expect(PafsCore::Project.count).to eq(0)
-  
+    end
+
+    it "deletes all project associated files" do
+      # run the task
+      Rake::Task[task_name].invoke
+
       # check if all associated files were deleted
       expect(Dir.glob("#{storage_path}/**/*").empty?).to be(true)
     end
