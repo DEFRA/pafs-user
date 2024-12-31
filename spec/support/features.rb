89362de404a29d99f04f0ddfcdd31eba79ee5d68
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-12-31 15:36:26 +0000

    Lint fixes

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join("spec/support/features/**/*.rb")].each { |f| require f }
+Rails.root.glob("spec/support/features/**/*.rb").each { |f| require f }
 
 RSpec.configure do |config|
   config.before(:suite) do
