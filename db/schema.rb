808bb4c64bf457c58b66a481a8a60fb6497d3268
Paul Doyle <97455399+PaulDoyle-EA@users.noreply.github.com>
2025-07-23 12:00:00 +0100

    Bump engine and other gem versions plus lint fixes (#880)
    https://eaflood.atlassian.net/browse/RUBY-3889

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_04_104418) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_02_180640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
@@ -330,6 +330,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_04_104418) do
     t.integer "earliest_start_year"
     t.string "updated_by_type"
     t.bigint "updated_by_id"
+    t.integer "pending_earliest_start_month"
+    t.integer "pending_earliest_start_year"
+    t.integer "pending_financial_year"
+    t.boolean "date_change_requires_confirmation", default: false, null: false
     t.index ["name"], name: "index_pafs_core_projects_on_name", unique: true
     t.index ["reference_number", "version"], name: "index_pafs_core_projects_on_reference_number_and_version", unique: true
     t.index ["slug"], name: "index_pafs_core_projects_on_slug", unique: true
